3f701a83857e94f4e6d02f76623f8df16cff41ee
xaerk <0124310622@alumnos.univa.mx>
2025-11-24 04:59:03 -0600

    added a whoooole bunch of stuff I don't really know how to use :c

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import pygame
+import os
+import freedraw
+pygame.init()
+
+screen = pygame.display.set_mode((854, 480))
+
+#importar assets
+asset_path = os.path.join("..","assets") #<- variable de la ruta a assets
+cursor_image = pygame.image.load(asset_path + "/tile_0027.png")
+#Esconder cursor en la pantalla para que esté el pixelado
+pygame.mouse.set_visible(False)
+# Mantener la ventana corriendo hasta que se presione la X
+running = True
+while running:
+    screen.fill((0,0,0))
+    freedraw.draw(screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    cursor_cords = pygame.mouse.get_pos()
+    screen.blit(cursor_image, cursor_cords)
+    pygame.display.flip()
+
+pygame.quit()
