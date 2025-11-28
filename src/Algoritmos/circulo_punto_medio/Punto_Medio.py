a7979180025503aa421648f27c643ba39ede2e03
xaerk <0124310622@alumnos.univa.mx>
2025-11-27 18:39:55 -0600

    for some reason the middle point ones didn't got pushed in the previous one

---
+++ src/Algoritmos/circulo_punto_medio/Punto_Medio.py
@@ -0,0 +1,34 @@
+import math
+import pygame
+pygame.init()
+
+
+def Circulo_Medio(surface,x,y,radio,color):
+
+    xc = 0
+    yc = radio
+    p = 1- radio
+
+    def dibujar_octante(surface,x,y,xc,yc,color):
+        surface.set_at((x + xc,y + yc),color)
+        surface.set_at((x - xc,y + yc), color)
+        surface.set_at((x + xc,y - yc), color)
+        surface.set_at((x - xc,y - yc), color)
+        surface.set_at((x + yc,y + xc), color)
+        surface.set_at((x - yc,y + xc), color)
+        surface.set_at((x + yc,y - xc), color)
+        surface.set_at((x - yc,y - xc), color)
+
+
+    while xc <= yc:
+        #dibujar
+        dibujar_octante(surface,x,y,xc,yc,color)
+        xc = xc + 1#actualizar x
+            #Parametro de edicion
+        if p < 0:
+            p = p + 2 * xc + 1 #se avanza solo x, y se mantiene
+        else:
+            yc = yc -1
+            p=p + 2 *(xc-yc)+1#avanzar y
+
+
